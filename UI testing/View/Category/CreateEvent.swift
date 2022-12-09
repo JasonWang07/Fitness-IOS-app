8353f234ee1ce9899a02f0316d8d3787f2c8c0d5
Jason Wang <jasonwang@JasondeMacBook-Air.local>
2022-12-09 15:48:25 +0800

    for Tim2

---
+++ UI testing/View/Category/CreateEvent.swift
@@ -0,0 +1,20 @@
+//
+//  CreateEvent.swift
+//  UI testing
+//
+//  Created by Jason Wang on 9/12/22.
+//
+
+import SwiftUI
+
+struct CreateEvent: View {
+    var body: some View {
+        
+    }
+}
+
+struct CreateEvent_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateEvent()
+    }
+}
